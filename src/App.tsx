6ce52cdf2d527fe4881af3ddc60212761a068d4a
MarkNielsen00 <mpnvip@gmail.com>
2023-02-19 22:08:01 +0100

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,68 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <div class="checkout">
+      <div class="total">
+        <p class="summary">Ordrehistorik</p>
+        <ul>
+          <li>(1) Stol - svanemærket et eller andet: <b>199 kr.</b></li>
+          <br></br>
+          <li>(1) Badering - Rød: <b>49 kr.</b></li>
+          <br></br>
+          <li>(1) Modelskib - Fra et eller andet fancy mærke: <b>499 kr.</b></li>
 
-    <div class="item">
-      <img src="https://images.bolia.com/cdn-cgi/image/background=%23f5f5f5,fit=pad,width=540,format=auto,height=405,quality=81/products/02-335-01_00004_angle.webp?v=1" 
-      width = "80px" height="80px"/>
-      <p class= "pItemHeader">Stol</p>
-      <p class="pItemPrice">Pris: </p>
-    </div>
+        </ul>
+        <div class="paySection">
+          <p class="totalSum"><b>Ordretotal: 800 kr</b></p>
+          <button class="pay" type="button">Betal</button>
+        </div>
+      </div>
 
-    <div class="total">
-      <p class="headerTotal">Total</p>
+      <div class="item">
+        <img src="https://images.bolia.com/cdn-cgi/image/background=%23f5f5f5,fit=pad,width=540,format=auto,height=405,quality=81/products/02-335-01_00004_angle.webp?v=1"
+          width="20%" height="100%" class="image2" />
+        <div class="itemText">
+          <p class="pItemHeader">Stol - svanemærket et eller andet</p>
+          <p class="pItemPrice">199 kr. </p>
+        </div>
+        <div class="amount">
+          <label for="amount">Antal</label>
+          <input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></input>
 
-      <p class="totalSum">Sum: </p>
-    </div>
+        </div>
+      </div>
+
+      <br></br>
+      <div class="item">
+        <img src="https://productimages.biltema.com/v1/Image/product/xlarge/2000043931/4"
+          width="20%" height="100%" class="image2" />
+        <i class="fas fa-trash"></i>
+        <div class="itemText">
+          <p class="pItemHeader">Badering - Rød</p>
+          <p class="pItemPrice">49 kr. </p>
+        </div>
+        <div class="amount">
+          <label for="amount">Antal</label>
+          <input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></input>
+
+        </div>
+      </div>
+
+      <br></br>
+      <div class="item">
+        <img src="https://www.lego.com/cdn/cs/set/assets/bltb94632aeb971eb91/10294.jpg?fit=bounds&format=jpg&quality=80&width=320&height=320&dpr=1"
+          width="20%" height="100%" class="image2" />
+        <div class="itemText">
+          <p class="pItemHeader">Modelskib - Fra et eller andet fancy mærke</p>
+          <p class="pItemPrice">499 kr. </p>
+        </div>
+        <div class="amount">
+          <label for="amount">Antal</label>
+          <input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></input>
+
+        </div>
+      </div>
 
     </div>
     /*<div className="App">
