cc32ad23ab8e319c36572d00b9e64067185dc2d0
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2023-02-20 15:06:53 +0100

    NameChanges

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
 
   return (
 
-    <div class="checkout">
-      <div class="total">
-        <p class="summary">Ordrehistorik</p>
-        <ul class="products">
+    <div className="checkout">
+      <div className="total">
+        <p className="summary">Ordrehistorik</p>
+        <ul className="products">
           <li>(1) Stol - svanemærket et eller andet: <b>199 kr.</b></li>
           <br></br>
           <li>(1) Badering - Rød: <b>49 kr.</b></li>
@@ -18,55 +18,55 @@ function App() {
           <li>(1) Modelskib - Fra et eller andet fancy mærke: <b><s>499 kr</s> 349 kr.</b></li>
 
         </ul>
-        <div class="paySection">
-          <p class="totalSum"><b>Ordretotal: 800 kr</b></p>
-          <p class="discount">Rabat: -150 kr</p>
-          <button class="pay" type="button">Betal</button>
+        <div className="paySection" >
+          <p className="totalSum"><b>Ordretotal: 800 kr</b></p>
+          <p className="discount">Rabat: -150 kr</p>
+          <button className="pay" type="button">Betal</button>
         </div>
       </div>
 
 <h2><b>Min indkøbskurv</b></h2>
-<hr class="line" width="600px"></hr>
-      <div class="item">
+<hr className="line" width="600px"></hr>
+      <div className="item">
         <img src="https://images.bolia.com/cdn-cgi/image/background=%23f5f5f5,fit=pad,width=540,format=auto,height=405,quality=81/products/02-335-01_00004_angle.webp?v=1"
-          width="20%" height="100%" class="image2" />
-        <div class="itemText">
-          <p class="pItemHeader">Stol - svanemærket et eller andet</p>
-          <p class="pItemPrice">199 kr. </p>
+          width="20%" height="100%" className="image2" />
+        <div className="itemText">
+          <p className="pItemHeader">Stol - svanemærket et eller andet</p>
+          <p className="pItemPrice">199 kr. </p>
         </div>
-        <div class="amount">
-          <label for="amount">Antal</label>
+        <div className="amount">
+          <label htmlFor="amount">Antal</label>
           <input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></input>
 
         </div>
       </div>
 
       <br></br>
-      <div class="item">
+      <div className="item">
         <img src="https://productimages.biltema.com/v1/Image/product/xlarge/2000043931/4"
-          width="20%" height="100%" class="image2" />
-        <i class="fas fa-trash"></i>
-        <div class="itemText">
-          <p class="pItemHeader">Badering - Rød</p>
-          <p class="pItemPrice">49 kr. </p>
+          width="20%" height="100%" className="image2" />
+        <i className="fas fa-trash"></i>
+        <div className="itemText">
+          <p className="pItemHeader">Badering - Rød</p>
+          <p className="pItemPrice">49 kr. </p>
         </div>
-        <div class="amount">
-          <label for="amount">Antal</label>
+        <div className="amount">
+          <label htmlFor="amount">Antal</label>
           <input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></input>
 
         </div>
       </div>
 
       <br></br>
-      <div class="item">
+      <div className="item">
         <img src="https://www.lego.com/cdn/cs/set/assets/bltb94632aeb971eb91/10294.jpg?fit=bounds&format=jpg&quality=80&width=320&height=320&dpr=1"
-          width="20%" height="100%" class="image2" />
-        <div class="itemText">
-          <p class="pItemHeader">Modelskib - Fra et eller andet fancy mærke</p>
-          <p class="pItemPrice"><s>499 kr.</s> 349 kr. </p>
+          width="20%" height="100%" className="image2" />
+        <div className="itemText">
+          <p className="pItemHeader">Modelskib - Fra et eller andet fancy mærke</p>
+          <p className="pItemPrice"><s>499 kr.</s> 349 kr. </p>
         </div>
-        <div class="amount">
-          <label for="amount">Antal</label>
+        <div className="amount">
+          <label htmlFor="amount">Antal</label>
           <input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></input>
 
         </div>
