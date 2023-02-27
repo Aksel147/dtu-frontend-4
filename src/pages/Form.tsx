042658be3ffa03d92446eae7858f528beb57bef3
Mark <s215782@student.dtu.dk>
2023-02-27 20:21:59 +0100

    minor changes

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -35,38 +35,40 @@ export default function Form() {
     }
 
     return (
-        <div className='form'>
-            <form>
-                <h1>Leveringsadresse</h1>
-                <input className='input-font' type='text' name='deliveryCountry' placeholder='Land' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryZipCode' placeholder='Postnr' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryCity' placeholder='By' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryAddressLine1' placeholder='Addresse Linje 1' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryAddressLine2' placeholder='Addresse Linje 2' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryName' placeholder='Navn' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryPhone' placeholder='Telefon' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryEmail' placeholder='Email' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryCompanyName' placeholder='Firmanavn' autoComplete='{false}' onChange={onChange} />
-                <input className='input-font' type='text' name='deliveryCompanyVAT' placeholder='VAT-nummer' autoComplete='{false}' onChange={onChange} />
+        <div className='formBody'>
+            <div className='form'>
+                <form>
+                    <h1>Leveringsadresse</h1>
+                    <input className='input-font' type='text' name='deliveryCountry' placeholder='Land' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryZipCode' placeholder='Postnr' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryCity' placeholder='By' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryAddressLine1' placeholder='Addresse Linje 1' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryAddressLine2' placeholder='Addresse Linje 2' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryName' placeholder='Navn' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryPhone' placeholder='Telefon' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryEmail' placeholder='Email' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryCompanyName' placeholder='Firmanavn' autoComplete='{false}' onChange={onChange} />
+                    <input className='input-font' type='text' name='deliveryCompanyVAT' placeholder='VAT-nummer' autoComplete='{false}' onChange={onChange} />
 
-                <br></br>
-                <h1>Betalingsadresse</h1>
-                <div className='check'>
-                    <label htmlFor='checkbox'>Samme som leveringsadresse</label>
-                    <input type="checkbox" value='false' name='checkbox' onChange={()=>setCheck(!check)} />
-                </div>
-                <input className='input-font' type='text' name='billingCountry' placeholder='Land' autoComplete='{false}' defaultValue={check ? state.deliveryCountry:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingZipCode' placeholder='Postnr' autoComplete='{false}' defaultValue={check ? state.deliveryZipCode:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingCity' placeholder='By' autoComplete='{false}' defaultValue={check ? state.deliveryCity:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingAddressLine1' placeholder='Addresse Linje 1' autoComplete='{false}' defaultValue={check ? state.deliveryAddressLine1:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingAddressLine2' placeholder='Addresse Linje 2' autoComplete='{false}' defaultValue={check ? state.deliveryAddressLine2:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingName' placeholder='Navn' autoComplete='{false}' defaultValue={check ? state.deliveryName:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingPhone' placeholder='Telefon' autoComplete='{false}' defaultValue={check ? state.deliveryPhone:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingEmail' placeholder='Email' autoComplete='{false}' defaultValue={check ? state.deliveryEmail:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingCompanyName' placeholder='Firmanavn' autoComplete='{false}' defaultValue={check ? state.deliveryCompanyName:""} onChange={onChange}/>
-                <input className='input-font' type='text' name='billingCompanyVAT' placeholder='VAT-nummer' autoComplete='{false}' defaultValue={check ? state.deliveryCompanyVAT:""} onChange={onChange} />
-                <input type="button" className='submit' value='Submit' />
-            </form>
+                    <br></br>
+                    <h1>Betalingsadresse</h1>
+                    <div className='check'>
+                        <label htmlFor='checkbox'>Samme som leveringsadresse</label>
+                        <input type="checkbox" value='false' name='checkbox' onChange={()=>setCheck(!check)} />
+                    </div>
+                    <input className='input-font' type='text' name='billingCountry' placeholder='Land' autoComplete='{false}' defaultValue={check ? state.deliveryCountry:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingZipCode' placeholder='Postnr' autoComplete='{false}' defaultValue={check ? state.deliveryZipCode:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingCity' placeholder='By' autoComplete='{false}' defaultValue={check ? state.deliveryCity:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingAddressLine1' placeholder='Addresse Linje 1' autoComplete='{false}' defaultValue={check ? state.deliveryAddressLine1:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingAddressLine2' placeholder='Addresse Linje 2' autoComplete='{false}' defaultValue={check ? state.deliveryAddressLine2:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingName' placeholder='Navn' autoComplete='{false}' defaultValue={check ? state.deliveryName:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingPhone' placeholder='Telefon' autoComplete='{false}' defaultValue={check ? state.deliveryPhone:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingEmail' placeholder='Email' autoComplete='{false}' defaultValue={check ? state.deliveryEmail:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingCompanyName' placeholder='Firmanavn' autoComplete='{false}' defaultValue={check ? state.deliveryCompanyName:""} onChange={onChange}/>
+                    <input className='input-font' type='text' name='billingCompanyVAT' placeholder='VAT-nummer' autoComplete='{false}' defaultValue={check ? state.deliveryCompanyVAT:""} onChange={onChange} />
+                    <input type="button" className='submit' value='Submit' />
+                </form>
+            </div>
         </div>
     );
 }
