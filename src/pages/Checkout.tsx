644285a9eebe665c4254affcc59381a17edb362c
Mark <s215782@student.dtu.dk>
2023-02-27 18:12:29 +0100

    Can now go to new page

---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,113 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Checkout.css';
+import products from '../assets/product.json';
+import CartItem from '../components/CartItem';
+import Item from '../models/Item';
+import Product from '../models/Product';
+
+export default function Checkout() {
+	const navigate = useNavigate();
+
+    function handleClick(event: any) {
+      navigate('form');
+    }
+	const defaultShoppingCart = [
+		{ productId: 'vitamin-c-500-250', quantity: 2, giftWrap: false },
+		{ productId: 'kids-songbook', quantity: 1, giftWrap: true },
+		{ productId: 'sugar-cane-1kg', quantity: 2, giftWrap: false },
+	];
+	const [shoppingCart, setShoppingCart] = useState<Item[]>(
+		defaultShoppingCart.map(
+			(item) =>
+				({
+					...item,
+					product: products.find(
+						(product) => product.id === item.productId
+					) as Product,
+				} as Item)
+		)
+	);
+
+	function setItemQuantity(productId: string, quantity: number) {
+		let newCart = [...shoppingCart];
+		const productIndex = newCart.findIndex(
+			(item) => item.product?.id === productId
+		);
+		if (productIndex !== -1) {
+			newCart[productIndex].quantity = quantity;
+		}
+		setShoppingCart(newCart);
+	}
+
+	function removeItem(productId: string) {
+		let newCart = [...shoppingCart];
+		const productIndex = newCart.findIndex(
+			(item) => item.product?.id === productId
+		);
+		newCart.splice(productIndex, 1);
+		setShoppingCart(newCart);
+	}
+
+	return (
+		<div className="checkout">
+				<div className="shopping-cart">
+					<h2>
+						<b>Min indkøbskurv</b>
+					</h2>
+
+					<hr />
+					{shoppingCart.map((item) => (
+						<CartItem
+							key={item.product?.id}
+							item={item}
+							setQuantity={setItemQuantity}
+							remove={removeItem} />
+					))}
+				</div>
+
+				<div className="total">
+					<p className="summary">Ordre</p>
+
+					<div className="summaryContainer">
+						<div className="placeOnLine">
+							<p className="totalSum">Subtotal: </p>
+							<p className="totalSumRight">
+								{shoppingCart.reduce(
+									(a, v) => (a = a + v.quantity * v.product?.price),
+									0
+								)}{' '}
+								DKK
+							</p>
+						</div>
+
+						<div className="placeOnLine">
+							<p className="discount">Rabat: </p>
+							<p className="discountRight"> -150 kr</p>
+						</div>
+
+						<hr />
+
+						<div className="placeOnLine">
+							<p className="totalSum">
+								<b>Pris i alt (inkl. moms): </b>
+							</p>
+							<p className="totalSumRight">
+								<b>
+									{shoppingCart.reduce(
+										(a, v) => (a = a + v.quantity * v.product?.price),
+										0
+									) - 150}{' '}
+									DKK
+								</b>
+							</p>
+						</div>
+					</div>
+
+					<button className="pay" type="button" onClick={handleClick}>
+						Betal
+					</button>
+				</div>
+			</div>
+	);
+}
