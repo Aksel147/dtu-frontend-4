57e8dcac007ff4ead1d03035add496ab277774e1
MarkNielsen00 <mpnvip@gmail.com>
2023-03-03 23:07:45 +0100

    rebate 10% done

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,66 @@ import Form from './pages/Form';
 
 export default function App() {
 	return (
-		<div className="App">
-		<Routes>
-		  <Route path="/" element={<Checkout />} />
-		  <Route path="form" element={<Form />} />
-		</Routes>
-	  </div>
+		<div className="checkout">
+			<div className="shopping-cart">
+				<h2>
+					<b>Min indkøbskurv</b>
+				</h2>
+
+				<hr />
+				{shoppingCart.map((item) => (
+					<CartItem
+						key={item.product?.id}
+						item={item}
+						setQuantity={setItemQuantity}
+						remove={removeItem}
+					/>
+				))}
+			</div>
+
+			<div className="total">
+				<p className="summary">Ordre</p>
+
+				<div className="summaryContainer">
+					<div className="placeOnLine">
+						<p className="totalSum">Subtotal: </p>
+						<p className="totalSumRight">
+							{shoppingCart.reduce(
+								(a, v) => (a = a + v.quantity * v.product?.price),
+								0
+							)}{' '}
+							DKK
+						</p>
+					</div>
+
+					<div className="placeOnLine">
+						<p className="discount">Rabat: </p>
+						<p className="discountRight"> -150 kr</p>
+					</div>
+
+					<hr />
+
+					<div className="placeOnLine">
+						<p className="totalSum">
+							<b>Pris i alt (inkl. moms): </b>
+						</p>
+						<p className="totalSumRight">
+							<b>
+								{shoppingCart.reduce(
+									(a, v) => (a = a + v.quantity * v.product?.price),
+									0
+								) - 150}{' '}
+								DKK
+							</b>
+						</p>
+					</div>
+				</div>
+
+				<button className="pay" type="button">
+					Betal
+				</button>
+			</div>
+		</div>
 	);
 }
 
