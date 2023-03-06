ed3718a550134368f6e25bd04eb04a65a8b9fdd6
Tobias Mayn <tobiasmayn@gmail.com>
2023-03-06 11:08:48 +0100

    integration test of app routing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import { describe, expect, it } from 'vitest';
 import App from './App';
+import Checkout from './pages/Checkout';
 
 describe(App.name, () => {
 	it('should render', () => {
-		render(<App />);
+		render(
+			<MemoryRouter initialEntries={['/pages/Checkout']}>
+				<Checkout></Checkout>
+			</MemoryRouter>
+		);
 		expect(screen.getByText('Min indkøbskurv')).toBeInTheDocument();
 	});
 });
