9eb0468cde9dbfcad0b55d23ef0e30d5d23a5fc0
Tobias Mayn <tobiasmayn@gmail.com>
2023-03-13 03:58:30 +0100

    tests for rebate and upsell option

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,61 +1,85 @@
-import './CartItem.css'
-import Item from '../models/Item'
+import Item from '../models/Item';
+import './CartItem.css';
 
 export default function CartItem(props: {
-    item: Item,
-    setQuantity: (id: string, val: number) => void,
-    remove: (id: string) => void,
-    upsell: (id: string) => void
+	item: Item;
+	setQuantity: (id: string, val: number) => void;
+	remove: (id: string) => void;
+	upsell: (id: string) => void;
 }) {
-    let totalPrice = props.item.product.price * props.item.quantity;
-    if (props.item.quantity >= props.item.product.rebateQuantity) {
-        totalPrice = totalPrice * (1 - props.item.product.rebatePercent / 100);
-    }
+	let totalPrice = props.item.product.price * props.item.quantity;
+	if (props.item.quantity >= props.item.product.rebateQuantity) {
+		totalPrice = totalPrice * (1 - props.item.product.rebatePercent / 100);
+	}
 
-    return (
-        <div className='item-container'>
-            <div className="item">
-                <div className='left-container'>
-                    <img src="https://images.bolia.com/cdn-cgi/image/background=%23f5f5f5,fit=pad,width=540,format=auto,height=405,quality=81/products/02-335-01_00004_angle.webp?v=1"
-                        width="120px" height="120px" className="image2" />
-                    <div className="itemText">
-                        <p className="pItemHeader">{props.item.product.name}</p>
-                        <p className="pItemPrice">{totalPrice.toLocaleString('da-DK')} {props.item.product.currency}</p>
-                    </div>
-                </div>
-                <div className='right-container'>
-                    <div className='itemText'>
-                        <button onClick={() => { props.remove(props.item.product.id) }}>
-                            Fjern
-                        </button>
-                        {props.item.quantity < props.item.product.rebateQuantity &&
-                            <div className='quantity-nudge'>
-                                køb {props.item.product.rebateQuantity - props.item.quantity} mere
-                                og spar {props.item.product.rebatePercent}%&nbsp;&nbsp;&nbsp;
-                            </div>
-                        }
-                        <div className='amount'>
-                            <label htmlFor="amount">
-                                Antal
-                            </label>
-                            <input id="amount" type="number" min="1" max="100" value={props.item.quantity} onChange={(e) => { props.setQuantity(props.item.product.id, parseInt(e.target.value)) }} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {props.item.product.upsellProduct &&
-                <div className='upsell'>
-                    <div>
-                        Opgrader?
-                        <br />
-                        {props.item.product.upsellProduct.name}
-                    </div>
-                    <button onClick={() => { props.upsell(props.item.product.id) }}>
-                        vælg
-                    </button>
-                </div>
-            }
-
-        </div>
-    )
+	return (
+		<div className="item-container">
+			<div className="item">
+				<div className="left-container">
+					<img
+						src="https://images.bolia.com/cdn-cgi/image/background=%23f5f5f5,fit=pad,width=540,format=auto,height=405,quality=81/products/02-335-01_00004_angle.webp?v=1"
+						width="120px"
+						height="120px"
+						className="image2"
+					/>
+					<div className="itemText">
+						<p className="pItemHeader">{props.item.product.name}</p>
+						<p className="pItemPrice">
+							{totalPrice.toLocaleString('da-DK')} {props.item.product.currency}
+						</p>
+					</div>
+				</div>
+				<div className="right-container">
+					<div className="itemText">
+						<button
+							onClick={() => {
+								props.remove(props.item.product.id);
+							}}
+						>
+							Fjern
+						</button>
+						{props.item.quantity < props.item.product.rebateQuantity && (
+							<div className="quantity-nudge">
+								køb {props.item.product.rebateQuantity - props.item.quantity}{' '}
+								mere og spar {props.item.product.rebatePercent}
+								%&nbsp;&nbsp;&nbsp;
+							</div>
+						)}
+						<div className="amount">
+							<label htmlFor="amount">Antal</label>
+							<input
+								id="amount"
+								type="number"
+								min="1"
+								max="100"
+								value={props.item.quantity}
+								onChange={(e) => {
+									props.setQuantity(
+										props.item.product.id,
+										parseInt(e.target.value)
+									);
+								}}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			{props.item.product.upsellProduct && (
+				<div className="upsell">
+					<div>
+						Opgrader?
+						<br />
+						{props.item.product.upsellProduct.name}
+					</div>
+					<button
+						onClick={() => {
+							props.upsell(props.item.product.id);
+						}}
+					>
+						vælg
+					</button>
+				</div>
+			)}
+		</div>
+	);
 }
---
+++ tests/integration/Checkout.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import { expect, it } from 'vitest';
+import App from '../../src/App';
+/*
+beforeEach(() => {
+    // Render Checkout page
+	render(<Checkout />);
+})*/
+
+it('check rebate on item: Rørsukker, 1000g', async () => {
+	render(<App />, { wrapper: BrowserRouter });
+	const quantityField = screen.getAllByDisplayValue(2)[1];
+	const price = screen.getByText('80 DKK');
+
+	await userEvent.clear(quantityField);
+	await userEvent.type(quantityField, '4');
+
+	expect(price).toHaveTextContent('120 DKK');
+});
+
+it('check upsell option on item: Rørsukker, 1000g ', async () => {
+	render(<App />, { wrapper: BrowserRouter });
+
+	const chooseButton = screen.getAllByText('vælg')[2];
+	await userEvent.click(chooseButton);
+	expect(screen.getByText('Rørsukker, økologisk, 1000g'));
+});
