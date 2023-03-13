d12eb620d05c9f3c8f42bbf600d299743df7c638
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2023-03-13 10:45:23 +0100

    Something wrong with tests

--- tests/playwright/playwrightPages/indexTest.ts
+++ tests/playwright/playwrightPages/indexTest.ts
@@ -2,11 +2,20 @@ import { Locator, Page, expect } from "@playwright/test";
 
 export class IndexTestPage {
     readonly page: Page;
-
-
+    readonly quantity_nudge1: Locator;
+    readonly item0: Locator;
+    readonly finalPrice: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.quantity_nudge1 = page.locator('.quantity-nudge').nth(0)
+        this.item0 = page.locator('.pItemHeader').nth(0)
+        this.finalPrice = page.locator('.totalSumRight').nth(1)
+
+    }
+
+    async ClickButton(string: string, number: number = 0) {
+        await this.page.getByRole('button', { name: `${string}` }).nth(number).click()
     }
 
 }
